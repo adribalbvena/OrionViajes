a436eecf82b5078db1ea78a36549cef8a85bfdcc
Adriana Balbuena <adribalbuena@hotmail.com>
2022-10-09 18:23:17 +0200

    refactor

--- app/src/main/java/ar/edu/ort/orionviajes/MainActivity.kt
+++ app/src/main/java/ar/edu/ort/orionviajes/MainActivity.kt
@@ -2,13 +2,8 @@ package ar.edu.ort.orionviajes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import ar.edu.ort.orionviajes.databinding.ActivityMainBinding
-import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,38 +26,6 @@ class MainActivity : AppCompatActivity() {
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        travelViewModel = ViewModelProvider(this).get(TravelViewModel::class.java)
-//
-//
-//        linearLayoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
-//        binding.travelsRecyclerView.apply {
-//            setHasFixedSize(true)
-//            layoutManager = linearLayoutManager
-//            adapter = travelRecyclerAdapter
-//        }
-//
-//        travelViewModel.getTravels()
-//
-//
-//
-//        travelViewModel.travels.observe(this, Observer {
-//            //travelRecyclerAdapter.submitList(it)
-//            when (it) {
-//                is Resource.Success -> {
-//                    travelRecyclerAdapter.submitList(it.data)
-//                }
-//                is Resource.Error -> {
-//                    //Snackbar.make(binding.root, it.message.toString() ,Snackbar.LENGTH_LONG).show()
-//                    val snackbar = Snackbar.make(binding.root, it.message.toString(), Snackbar.LENGTH_LONG)
-//                    snackbar.setAction("Reintentar") {
-//                        binding.root
-//                    }
-//                    snackbar.show()
-//                }
-//
-//            }
-//
-//        })
     }
 
 }
--- app/src/main/java/ar/edu/ort/orionviajes/MyTravelsFragment.kt
+++ app/src/main/java/ar/edu/ort/orionviajes/MyTravelsFragment.kt
@@ -5,10 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.get
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import ar.edu.ort.orionviajes.databinding.FragmentMyTravelsBinding
@@ -30,11 +28,6 @@ class MyTravelsFragment : Fragment() {
     }
 
     private lateinit var linearLayoutManager: LinearLayoutManager
-
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//
-//    }
 
 
     override fun onCreateView(
