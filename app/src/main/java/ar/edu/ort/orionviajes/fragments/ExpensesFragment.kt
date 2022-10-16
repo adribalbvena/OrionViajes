b2b3e215c403d8287baeacc9c822c22843fa5f3f
Adriana Balbuena <adribalbuena@hotmail.com>
2022-10-16 15:57:22 +0200

    fixes

--- app/src/main/java/ar/edu/ort/orionviajes/fragments/ExpensesFragment.kt
+++ app/src/main/java/ar/edu/ort/orionviajes/fragments/ExpensesFragment.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import ar.edu.ort.orionviajes.ExpenseViewModel
@@ -17,8 +19,8 @@ import kotlin.math.exp
 
 
 class ExpensesFragment : Fragment() {
-    private var _binding : FragmentExpensesBinding? = null
-    private val binding get() =  _binding!!
+    private lateinit var binding: FragmentExpensesBinding
+    //private val binding get() =  _binding!!
 
     private lateinit var expenseRecyclerAdapter: ExpenseRecyclerAdapter
 
@@ -26,21 +28,13 @@ class ExpensesFragment : Fragment() {
 
     private lateinit var linearLayoutManager: LinearLayoutManager
 
-    //private val travel = ExpensesFragmentArgs.fromBundle(requireArguments()).travelId
-
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        _binding = FragmentExpensesBinding.inflate(inflater, container, false)
+        binding = FragmentExpensesBinding.inflate(inflater, container, false)
         val view = binding.root
 
         val travel = ExpensesFragmentArgs.fromBundle(requireArguments()).travelId
@@ -53,17 +47,28 @@ class ExpensesFragment : Fragment() {
             binding.btnScanExpense.visibility = View.VISIBLE
         }
 
+        binding.btnFormExpense.setOnClickListener{
+            val action = ExpensesFragmentDirections.actionExpensesFragmentToCreateExpenseFragment()
+            view.findNavController().navigate(action)
+        }
+
         return view
     }
 
     private fun initExpensesRecyclerView(){
         linearLayoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
-        binding.expensesRecyclerView.apply {
-            setHasFixedSize(true)
-            layoutManager = linearLayoutManager
-            expenseRecyclerAdapter = ExpenseRecyclerAdapter()
-            adapter = expenseRecyclerAdapter
-        }
+
+        binding.expensesRecyclerView.setHasFixedSize(true)
+        binding.expensesRecyclerView.layoutManager = linearLayoutManager
+        expenseRecyclerAdapter = ExpenseRecyclerAdapter()
+        binding.expensesRecyclerView.adapter = expenseRecyclerAdapter
+
+//        binding.expensesRecyclerView.apply {
+//            setHasFixedSize(true)
+//            layoutManager = linearLayoutManager
+//            expenseRecyclerAdapter = ExpenseRecyclerAdapter()
+//            adapter = expenseRecyclerAdapter
+//        }
     }
 
     fun initExpenseViewModel(travel_id: String) {
@@ -72,7 +77,7 @@ class ExpensesFragment : Fragment() {
             if (it == null) {
                 Snackbar.make(binding.root, "Error al cargar los gastos" , Snackbar.LENGTH_LONG).show()
             } else {
-                expenseRecyclerAdapter.submitList(it)
+                expenseRecyclerAdapter.updateList(it)
             }
         })
 
